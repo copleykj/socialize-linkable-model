baaea457b5470d3dc775e0994e15aabe604c6fc6
copleykj <copleykj@gmail.com>
2016-02-12 13:03:04 -0500

    minor version patch

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:linkable-model",
     summary: "A package allowing linking of records in one collection with records from many other collections",
-    version: "0.2.1",
+    version: "0.3.0",
     git: "https://github.com/copleykj/socialize-linkable-model.git"
 });
 
