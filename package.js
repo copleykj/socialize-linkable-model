cc58d54e2ced2ec1c7135ace0d26e863c39060be
copleykj <copleykj@gmail.com>
2016-04-03 23:34:01 -0400

    Still need BaseModel oops :)

--- package.js
+++ package.js
@@ -1,12 +1,16 @@
 Package.describe({
     name: "socialize:linkable-model",
     summary: "A package allowing linking of records in one collection with records from many other collections",
-    version: "0.4.0",
+    version: "0.4.1",
     git: "https://github.com/copleykj/socialize-linkable-model.git"
 });
 
 Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
+
+    api.use("socialize:base-model@0.4.0");
+
+    api.imply("socialize:base-model");
 
     //Add the friend-model files
     api.addFiles("common/linkable-model.js");
