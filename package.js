6a4a542f0a7ec7580e9aa34a181547d789073501
copleykj <copleykj@gmail.com>
2016-05-22 23:54:06 -0400

    update package.js

--- package.js
+++ package.js
@@ -1,22 +1,17 @@
 Package.describe({
     name: "socialize:linkable-model",
     summary: "A package allowing linking of records in one collection with records from many other collections",
-    version: "0.4.1",
+    version: "1.0.0",
     git: "https://github.com/copleykj/socialize-linkable-model.git"
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("1.0.2.1");
+    api.versionsFrom("1.3");
 
     api.use("ecmascript");
 
-    api.use("socialize:base-model@1.0.0");
+    api.use(["socialize:base-model@1.0.0"]);
+    api.imply(["socialize:base-model"]);
 
-    api.imply("socialize:base-model");
-
-    //Add the friend-model files
-    api.addFiles("common/linkable-model.js");
-
-
-    api.export(["LinkableModel"]);
+    api.mainModule("common/linkable-model.js");
 });
