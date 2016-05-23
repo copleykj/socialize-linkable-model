6b9fb571c1746d5a299c6ebd340b7fb1c6567e7a
copleykj <copleykj@gmail.com>
2016-05-22 23:53:40 -0400

    make linkable abstract

--- common/linkable-model.js
+++ common/linkable-model.js
@@ -1,36 +1,28 @@
-/**
- * A scaffold for creating models which can link records from one collection to records from many other collections
- * @class LinkableModel
- */
-LinkableModel = {};
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+//a place to store references to the collections where the commentable objects are stored.
+let LinkableTypes = {};
 
 /**
- * The database object
- * @returns {Instance} An instance of varying types depending on what the comment linked to
+ * A scaffold for creating models which can link records from one collection to records from many other collections
+ * @mixin LinkableModel
  */
-var linkableMethods = {
-    linkedObject: function () {
+export let LinkableModel = Base => class extends Base {
+    constructor(document) {
+        super(document)
+    }
+
+
+    /**
+     * linkedObject - Get the model for the linked record
+     *
+     * @return {Model}  A model of varying types depending on the linked objects type
+     */
+    linkedObject() {
         var collection = LinkableModel.getCollectionForRegisteredType(this.objectType);
         return collection.findOne(this.linkedObjectId);
     }
-}
 
-//a place to store references to the collections where the commentable objects are stored.
-var LinkableTypes = {};
-
-LinkableModel.makeLinkable = function(model) {
-    _.extend(model.prototype, linkableMethods);
-}
-
-/**
- * Register a data type that can be commented on storing its collection so we can find the object later
- * @param {String}           type       The name of the type
- * @param {Mongo.Collection} collection The collection where the type of data is stored
- */
-LinkableModel.registerLinkableType = function (model, type) {
-    model.prototype._objectType = type;
-
-    LinkableTypes[type] = model.prototype.getCollection();
 };
 
 /**
@@ -38,8 +30,21 @@ LinkableModel.registerLinkableType = function (model, type) {
  * @param   {String}           type The name of the data type
  * @returns {Mongo.Collection} The Collection where the type of data is stored
  */
-LinkableModel.getCollectionForRegisteredType = function (type) {
+LinkableModel.getCollectionForRegisteredType = function(type) {
     return LinkableTypes[type];
+};
+
+/**
+ * Register a data type that can be commented on storing its collection so we can find the object later
+ * @param {BaseModel}           type       The name of the type
+ * @param {Mongo.Collection} collection The collection where the type of data is stored
+ */
+LinkableModel.registerLinkableType = function(model) {
+    let type = model.prototype.getCollectionName();
+
+    model.prototype._objectType = type;
+
+    LinkableTypes[type] = model.prototype.getCollection();
 };
 
 LinkableModel.LinkableSchema = new SimpleSchema({
@@ -49,7 +54,6 @@ LinkableModel.LinkableSchema = new SimpleSchema({
     },
     "objectType":{
         type:String,
-        optional:true,
         denyUpdate:true,
     }
 });
