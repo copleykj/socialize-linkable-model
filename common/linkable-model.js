08c2701da425e21564e7deda765468a959550b2f
StorytellerCZ <storyteller@freedombase.net>
2017-06-03 15:39:53 +0200

    Import simpl-schema

--- common/linkable-model.js
+++ common/linkable-model.js
@@ -1,4 +1,4 @@
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import SimpleSchema from 'simpl-schema';
 
 //a place to store references to the collections where the commentable objects are stored.
 let LinkableTypes = {};
