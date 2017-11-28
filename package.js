21e80263cf2aa4e2956412d09d037456e643143c
Kelly Copley <copleykj@gmail.com>
2017-11-28 10:06:31 -0500

    single quotes

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 /* global Package */
 Package.describe({
-    name: "socialize:linkable-model",
-    summary: "A package allowing linking of records in one collection with records from many other collections",
-    version: "1.0.1",
-    git: "https://github.com/copleykj/socialize-linkable-model.git"
+    name: 'socialize:linkable-model',
+    summary: 'A package allowing linking of records in one collection with records from many other collections',
+    version: '1.0.1',
+    git: 'https://github.com/copleykj/socialize-linkable-model.git',
 });
 
 Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
-    api.use(["socialize:base-model@1.1.0"]);
-    api.imply(["socialize:base-model"]);
+    api.use(['socialize:base-model@1.1.0']);
+    api.imply(['socialize:base-model']);
 
     api.mainModule('common/common.js');
 });
