505846e035119ba3c35933cfaaaf963e3b582446
Kelly Copley <copleykj@gmail.com>
2020-08-06 16:33:34 -0400

    update versionsFrom to 1.10.2

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.3');
+    api.versionsFrom('1.10.2');
 
     api.use(['socialize:base-model@1.1.4']);
     api.imply(['socialize:base-model']);
