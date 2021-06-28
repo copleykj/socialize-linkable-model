53f85b37d46652556846bd7c541c10d5466093f2
Kelly Copley <copleykj@gmail.com>
2021-06-27 23:55:14 -0400

    update deps and versionsFrom for compatibility

--- package.js
+++ package.js
@@ -2,14 +2,14 @@
 Package.describe({
     name: 'socialize:linkable-model',
     summary: 'A package allowing linking of records in one collection with records from many other collections',
-    version: '1.0.5',
+    version: '1.0.6',
     git: 'https://github.com/copleykj/socialize-linkable-model.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.10.2');
+    api.versionsFrom(['1.10.2', '2.3']);
 
-    api.use(['socialize:base-model@1.1.5']);
+    api.use(['socialize:base-model@1.1.7']);
     api.imply(['socialize:base-model']);
 
     api.mainModule('common/common.js');
